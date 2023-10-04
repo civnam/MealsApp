65d3c4209362ff0444261edd084ee16c8193c435
Victor Dimas <civnam@outlook.com>
2023-10-03 18:03:55 -0600

    [feature] - Sliding intro cards added

--- MealsApp/MealsApp/AppDelegate.swift
+++ MealsApp/MealsApp/AppDelegate.swift
@@ -15,16 +15,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     private let viewModel = MealsViewModel()
     @State private var eventManager = EventManager()
+    private var firstTimeUserApp: Bool = true
+    private var completion: Callback?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-
+        
         let tabBarController = setupTabBarController()
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.rootViewController = tabBarController
-        self.window?.makeKeyAndVisible()
+        
+        let firstTimeKey = UserDefaults.standard.dictionaryRepresentation().keys.contains("isFirstTimeUserApp")
+        
+        if firstTimeKey {
+            self.firstTimeUserApp = UserDefaults.standard.object(forKey: "isFirstTimeUserApp") as? Bool ?? false
+        }
+        
+        self.completion = { _ in
+            self.goToDashboard(tabBarController: tabBarController)
+        }
+        
+        if firstTimeUserApp {
+            let introCardsVC = getIntroSlidingCardsView()
+            self.window?.rootViewController = introCardsVC
+            self.window?.makeKeyAndVisible()
+        } else {
+            goToDashboard(tabBarController: tabBarController)
+        }
         
         return true
+    }
+    
+    private func goToDashboard(tabBarController: MainTabBarViewController) {
+        
+        UIView.animate(withDuration: 2, animations: {
+            self.window?.rootViewController = tabBarController
+            self.window?.makeKeyAndVisible()
+        })
     }
     
     private func setupTabBarController() -> MainTabBarViewController{
@@ -49,6 +74,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                                         calendarViewController: calendarVC)
         tabBarController.selectedIndex = 1
         return tabBarController
+    }
+    
+    private func getIntroSlidingCardsView() -> UIViewController {
+        
+        var introCardsView = IntroCardsView()
+        introCardsView.completion = self.completion
+        let introCardsVC = UIHostingController(rootView: introCardsView)
+        introCardsVC.view.backgroundColor = .clear
+        introCardsVC.modalPresentationStyle = .fullScreen
+        return introCardsVC
     }
 }
 
--- MealsApp/MealsApp/Utilities/CallBacks.swift
+++ MealsApp/MealsApp/Utilities/CallBacks.swift
@@ -8,3 +8,4 @@
 import Foundation
 
 typealias Completion = (String, String) -> Void
+typealias Callback = (Bool) -> Void
